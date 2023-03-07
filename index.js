0e0da77b06ad5f03635257469fd4c61a6d8e8d9d
MinaMouraly <minamouraly28@gmail.com>
2023-03-07 11:08:56 +0100

    web serveur

---
+++ index.js
@@ -0,0 +1,19 @@
+const express =require("express");
+const app = express();//initialiation 
+const port = 3000;
+
+function main(){
+
+    app.get("/", (req,res) => {
+        //const dt= new Data();
+        res.send("Hello Toto je m'appele mina !");
+        //console.log(dt);
+    });
+
+    app.listen(port,function() {
+        console.log('Serveur lancé sur port ${port}');
+    });
+    
+}
+main();
+
