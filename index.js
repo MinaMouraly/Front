e880534c61b9417852ca318bb5ca70fd6ac79606
MinaMouraly <minamouraly28@gmail.com>
2023-03-09 08:09:05 +0100

    creation de client

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ function main(){
         //req contient des info sur la requete HTTP 
         //res est la reponde de la fonction renvoyer au client
         res.set('Content-Type', 'text/html');//permet de fixer la valeur d'un champs de l'entete HTTP
-        res.send('<html><body><h1>Accueil</h1></body></html>');//permet de transmettre une reponse au client 
-       
-    
+        res.send('<html><body><h1>Accueil</h1></body></html>');//permet de transmettre une reponse au client
+
     });
 
     //page 1 contient la liste des clients
@@ -22,12 +21,20 @@ function main(){
 
         res.set('Content-Type', 'application/json');
         //res.send( __dirname + '.html');
+     
+    });
+
+     //page 2 contient  la creation  des clients
+     app.post("/liste.html", (req,res) => {
+
+        res.set('Content-Type', 'text/html');
+        res.send('<html><body><h1>Bienvenue au formulaire </h1></body></html>')
        
     
     });
 
-    app.listen(port,function() {
-        console.log('Serveur lancé sur http.//localhost: ${port}');
+    app.listen(port,function() {//lance le serveur web est a l'ecoute 
+        console.log(`Serveur lancé sur http.//localhost: ${port}`);
     });
   
     // on pourra creer une page 
